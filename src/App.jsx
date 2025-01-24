af44b01f1a163c35a053aef00d5847b0db343ca7
IvanR <ivan.ramirezf@gmail.com>
2025-01-25 00:09:21 +1000

    Created class with the same name in 2 different files.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing.jsx";
 import Product from "./pages/Product";
-import PageNotFound from "./pages/PageNotFound.jsx";
+import AppLayout from "./pages/AppLayout.jsx";
 
 function App() {
   return (
@@ -11,7 +11,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="product" element={<Product />} />
+        <Route path="app" element={<AppLayout />} />
       </Routes>
     </BrowserRouter>
   );
