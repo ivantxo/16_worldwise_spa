c30fbea7d7d94de137a3dacd18230c068255e050
IvanR <ivan.ramirezf@gmail.com>
2025-01-27 23:25:23 +1000

    Changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing.jsx";
 import Product from "./pages/Product";
 import AppLayout from "./pages/AppLayout.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import Login from "./pages/Login.jsx";
+import "./index.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="product" element={<Product />} />
-        <Route path="product" element={<Product />} />
         <Route path="app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
